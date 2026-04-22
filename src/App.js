7c1abbf8a35eb8157819df28e02a464b1ca712a8
Victor Oliveira <victor.a.oliveira16@aluno.senai.br>
2026-04-22 16:40:10 -0300

    Trabalhando com react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './componentes/Header';
+import styled from 'styled-components';
+
+const AppContainer = ` 
+  width: 100vw;
+  height: 100vw;
+  background-image: linear-gradient(90deg, #002f52 35%, #326589);
+
+
+li {
+  list-style: none;
+}
+
+.App-logo {
+  height: 40vmin;
+  pointer-events: none;
+}
+
+
+`
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppContainer>
+      <Header />
+    </AppContainer>
   );
 }
 
